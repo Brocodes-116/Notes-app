e92b74ea99af729d3b4c623eec7977ab1b0b98e8
Brocodes-116 <shubhang.works.116@gmail.com>
2026-03-16 17:16:56 +0530

    Added View feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
   const [Tasks, setTasks] = useState([]);
   const [edit, setedit] = useState(false);
   const [editIndex, seteditIndex] = useState(-1);
+  const [View, setView] = useState(false)
+  const [Vdata, setVdata] = useState({})
+  const [Backdrop, setBackdrop] = useState(false)
+
   const handleDelete = (index) => {
     let copiedData = [...Tasks];
     copiedData.splice(index, 1);
@@ -19,8 +23,11 @@ function App() {
     const dup = Tasks[idx];
     setTitle(dup.Title)
     setDesc(dup.Desc)
-
     seteditIndex(idx);
+  }
+  const handleView = (idx) => {
+    const dup = Tasks[idx];
+    setVdata(dup);
   }
 
   const handleEditIndex = () => {
@@ -39,7 +46,7 @@ function App() {
   return (
     <>
       <div className={`bg-zinc-800 text-white w-full min-h-screen p-10`}>
-        <div className="max-w-[1200px] mx-auto my-0 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
+        <div className="max-w-[1500px] mx-auto my-0 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
           <div
             onClick={() => {
               setShow(s => !s);
@@ -53,7 +60,7 @@ function App() {
             <div className="fixed inset-0 z-50 flex items-center justify-center">
               {/* BACKDROP */}
               <div
-                className="absolute inset-0 bg-zinc-700/80"
+                className="fixed inset-0 bg-zinc-700/80"
                 onClick={() => setShow(false)} // close modal when clicking outside
               ></div>
 
@@ -103,6 +110,11 @@ function App() {
           {Tasks.map((items, index) => {
             return (
               <div key={index}
+              onClick={() => {
+                  handleView(index)
+                  setView(true)
+                }
+                }
                 className="card min-h-30 bg-zinc-700 rounded-xl border-3 border-solid border-white/40 flex flex-col justify-center items-start cursor-pointer hover:scale-105 duration-400 p-5 gap-1">
                 <div
                   className="w-full flex justify-between">
@@ -114,7 +126,7 @@ function App() {
                       onClick={() => {
                         handleDelete(index);
                       }}
-                      className="w-9 h-9 bg-amber-100 text-red-700 text-xl flex justify-center items-center rounded">
+                      className="w-7 h-7 bg-cyan-200 text-red-700 text-xl flex justify-center items-center rounded">
                       <i className="fa-solid fa-x"></i>
                     </span>
                   </div>
@@ -125,10 +137,27 @@ function App() {
 
                 <div
                   className="w-full flex justify-end ">
-                  <div
-                    className="hover:bg-amber-300 hover:text-black p-1 transition-colors duration-300 rounded"
-                    onClick={() => { handleEdit(index) }}>
-                    <i class="fa-solid fa-pen-to-square"></i>
+                  <div className="w-[20%] flex justify-between">
+                    {/* View Button */}
+                    <div
+                      className="hover:bg-green-300 hover:text-black p-1 transition-colors duration-300 rounded"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleView(index)
+                        setView(true)
+                      }
+                    }
+                    >
+                      <i class="fa-solid fa-eye"></i>
+                    </div>
+                    {/* Edit Button */}
+                    <div className="hover:bg-amber-300 hover:text-black p-1 transition-colors duration-300 rounded"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleEdit(index)
+                      }}>
+                      <i class="fa-solid fa-pen-to-square"></i>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -139,7 +168,7 @@ function App() {
             {/* BACKDROP */}
             <div
               className="absolute inset-0 bg-zinc-700/80"
-              onClick={() => setedit(false)} // close modal when clicking outside
+              onClick={() => setBackdrop(true)} // close modal when clicking outside
             ></div>
 
             {/* MODAL */}
@@ -147,7 +176,7 @@ function App() {
               <h3 className="text-xl text-zinc-200 font-bold pb-3">Edit Note</h3>
               <form
                 onSubmit={(e) => {
-
+                  
 
                 }}
                 className="flex flex-col items-center gap-3"
@@ -169,18 +198,51 @@ function App() {
                 <button
                   onClick={(e) => {
                     e.preventDefault();
-                    handleEditIndex()
+                    handleEditIndex();
+                    setBackdrop(false)
                     console.log(Title, Desc)
                   }}
                   className="bg-blue-500 w-full font-bold tracking-wider py-2 rounded-xl cursor-pointer">
                   Save Changes
                 </button>
+                {Backdrop && <p className="text-red-600 text-center">Edit the content or click on "save changes" button❗❗❗</p>}
               </form>
             </div>
           </div>
           }
 
+
         </div>
+
+        {View &&
+          <div>
+            {/* BackDrop */}
+            <div className="fixed inset-0 bg-gray-800/70 min-h-96"></div>
+
+            {/* Main container that shows the content */}
+
+            <div className="mx-auto w-[90%] lg:w-[80%] absolute top-20 left-6 sm:left-10 lg:left-28 h-[600px]">
+              <div className="h-full w-full  flex">
+                <div className="h-full w-[80%] bg-black rounded-l-2xl p-4">
+                  <div className=" h-full w-full border-2 border-t-0 border-solid border-white overflow-y-auto relative">
+                    <h3 className="text-2xl font-semibold border p-2 border-l-0 border-r-0 border-amber-50 sticky top-0 bg-black"><span className="text-3xl">Title : </span> "{Vdata.Title}"</h3>
+                    <p className=" p-2 wrap-break-word"><span className="text-2xl"> Description : </span>"{Vdata.Desc}"</p>
+                  </div>
+                </div>
+                <div className="h-full w-[20%] bg-cyan-900 rounded-r-2xl">
+                  <div
+                    className="h-[8%] w-full  relative"
+                    onClick={() =>
+                      setView(false)
+                    }
+                  >
+                    <span className="absolute right-2 top-2 bg-fuchsia-400 p-1 hover:scale-110 cursor-pointer active:scale-100">❌</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
       </div>
     </>
   )
